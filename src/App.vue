6e92f4e5236bad08cf8831d8c54ecd2549f844bf
Shah Zaib <shahzaibk.ch@gmail.com>
2024-11-19 14:17:16 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <signupform />
+</template>
+
+<script>
+import Signupform from './components/Signupform.vue';
+export default {
+  name: 'App',
+  components: {
+    Signupform
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
